25ada4d71454cd88e15ab5a247477934f2e8b8c9
Tom Quant <tom@Toms-MacBook-Air.local>
2018-06-26 10:50:48 +1000

    second logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var gridArray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-var gridArray2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 var moveCount = 0
 var isWinner = false
 var turn = 1
@@ -7,14 +6,15 @@ var user1Score = 0
 var user2Score = 0
 var token1 = 'O'
 var token2 = "X"
-var boardSize = 3
+var boardSize = 0
 
 const init = function() {
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < gridArray.length; i++) {
     $('.box').text('')
   }
-  gridArray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  gridArray2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  for (var i = 0; i < gridArray.length; i++) {
+    gridArray[i] = 0
+  }
   $('#sideRight').html('')
   moveCount = 0
   isWinner = false
@@ -47,18 +47,18 @@ const checkWinner = function() {
   }
 }
 const checkWinner2 = function() {
-  let g2 = gridArray2
+  let g = gridArray
   if (
-    g2[0] == g2[1] && g2[1] == g2[2] && g2[2] == g2[3] && g2[0] > 0 ||
-    g2[4] == g2[5] && g2[5] == g2[6] && g2[6] == g2[7] && g2[4] > 0 ||
-    g2[8] == g2[9] && g2[9] == g2[10] && g2[10] == g2[11] && g2[8] > 0 ||
-    g2[12] == g2[13] && g2[13] == g2[14] && g2[14] == g2[15] && g2[12] > 0 ||
-    g2[0] == g2[4] && g2[4] == g2[8] && g2[8] == g2[12] && g2[0] > 0 ||
-    g2[1] == g2[5] && g2[5] == g2[9] && g2[9] == g2[13] && g2[1] > 0 ||
-    g2[2] == g2[6] && g2[6] == g2[10] && g2[10] == g2[14] && g2[2] > 0 ||
-    g2[3] == g2[7] && g2[7] == g2[11] && g2[11] == g2[15] && g2[3] > 0 ||
-    g2[0] == g2[5] && g2[5] == g2[10] && g2[10] == g2[15] && g2[0] > 0 ||
-    g2[3] == g2[6] && g2[6] == g2[9] && g2[9] == g2[12] && g2[3] > 0
+    g[0] == g[1] && g[1] == g[2] && g[2] == g[3] && g[0] > 0 ||
+    g[4] == g[5] && g[5] == g[6] && g[6] == g[7] && g[4] > 0 ||
+    g[8] == g[9] && g[9] == g[10] && g[10] == g[11] && g[8] > 0 ||
+    g[12] == g[13] && g[13] == g[14] && g[14] == g[15] && g[12] > 0 ||
+    g[0] == g[4] && g[4] == g[8] && g[8] == g[12] && g[0] > 0 ||
+    g[1] == g[5] && g[5] == g[9] && g[9] == g[13] && g[1] > 0 ||
+    g[2] == g[6] && g[6] == g[10] && g[10] == g[14] && g[2] > 0 ||
+    g[3] == g[7] && g[7] == g[11] && g[11] == g[15] && g[3] > 0 ||
+    g[0] == g[5] && g[5] == g[10] && g[10] == g[15] && g[0] > 0 ||
+    g[3] == g[6] && g[6] == g[9] && g[9] == g[12] && g[3] > 0
   ) {
     if (turn === 2) {
       user1Score++
@@ -78,6 +78,10 @@ const checkWinner2 = function() {
 $(document).ready(function() {
 
   $('#buttonFour').on('click', function() {
+    if(gridArray.length === 9){
+      gridArray.push(0,0,0,0,0,0,0)
+    }
+    if (boardSize !== 4){
     if (boardSize === 3) {
       for (var i = 1; i < 8; i++) {
         $('#grid').append(`<div id="${i + 9}" class="box" ></div>`)
@@ -86,9 +90,23 @@ $(document).ready(function() {
       $('#grid').addClass('grid-container4')
       boardSize = 4
     }
+    else if (boardSize === 0) {
+      for (var i = 1; i < 17; i++) {
+        $('#grid').append(`<div id="${i}" class="box" ></div>`)
+      }
+      $('#grid').removeClass('grid-container3')
+      $('#grid').addClass('grid-container4')
+      boardSize = 4
+    }
+  }
+
   })
 
   $('#buttonThree').on('click', function() {
+    if (gridArray.length === 16){
+      gridArray.splice(9)
+    }
+if (boardSize !== 3){
     if (boardSize === 4) {
       for (var i = 1; i < 8; i++) {
         $(`#${i+9}`).remove()
@@ -97,6 +115,15 @@ $(document).ready(function() {
       $('#grid').addClass('grid-container3')
       boardSize = 3
     }
+    else if (boardSize === 0) {
+      for (var i = 1; i < 10; i++) {
+        $('#grid').append(`<div id="${i}" class="box" ></div>`)
+      }
+      $('#grid').removeClass('grid-container4')
+      $('#grid').addClass('grid-container3')
+      boardSize = 3
+    }
+  }
   })
 
   $('div').delegate('.box', 'click', function() {
@@ -129,16 +156,16 @@ $(document).ready(function() {
 
       if (isWinner === true) {
         return
-      } else if (gridArray2[(this.id) - 1] > 0) {
+      } else if (gridArray[(this.id) - 1] > 0) {
       } else {
         if (turn === 1) {
           $(this).text(token1)
-          gridArray2.splice((this.id - 1), 1, 1)
+          gridArray.splice((this.id - 1), 1, 1)
           turn = 2
           moveCount++
         } else {
           $(this).text(token2)
-          gridArray2.splice((this.id - 1), 1, 2)
+          gridArray.splice((this.id - 1), 1, 2)
           turn = 1
           moveCount++
         }
